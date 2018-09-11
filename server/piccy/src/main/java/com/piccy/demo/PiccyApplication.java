c256426756fb1076e00eeb63c70e8d0551607b57
chriskondo <ckon6951@uni.sydney.edu.au>
2018-09-11 18:09:50 +1000

    Starter projected added for server

---
+++ server/piccy/src/main/java/com/piccy/demo/PiccyApplication.java
@@ -0,0 +1,12 @@
+package com.piccy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiccyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiccyApplication.class, args);
+	}
+}
