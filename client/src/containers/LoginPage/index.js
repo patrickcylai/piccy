b848ad4663ed669e3a98a3384ebf7a902e94c883
Patrick Lai <patrickcylai@gmail.com>
2018-10-05 22:59:36 +1000

    feature: redesign login page
     * changed gradient
     * added animation

--- client/src/containers/LoginPage/index.js
+++ client/src/containers/LoginPage/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
+import Fade from '@material-ui/core/Fade';
 
 import logo from '../../images/logo.svg';
 
@@ -10,17 +11,20 @@ const styles = theme => ({
     display: 'flex',
     height: '100vh',
     justifyContent: 'center',
-    background: 'linear-gradient(90deg, #2980B9 0%, #6DD5FA 50%, #FFFFFF 100%)'
+    background:
+      'linear-gradient(60deg, #845EC2 0%, #D65DB1 10%, #FF6F91 20%, #FF9671 50%, #FFC75F 95%, #F9F871 100%)'
   },
   titleContainer: {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
-    textAlign: 'center'
+    textAlign: 'center',
+    color: 'white'
   },
   logo: {
     height: '230px',
-    marginBottom: '30px'
+    marginBottom: '30px',
+    filter: 'invert(100%)'
   }
 });
 
@@ -30,13 +34,17 @@ class LoginPage extends Component {
 
     return (
       <div className={classes.root}>
-        <div className={classes.titleContainer}>
-          <img className={classes.logo} src={logo} alt="logo" />
-          <Typography variant="display2">Welcome to Piccy.</Typography>
-          <Typography variant="subheading" gutterBottom>
-            Image sharing for everyone
-          </Typography>
-        </div>
+        <Fade in={true} timeout={3000}>
+          <div className={classes.titleContainer}>
+            <img className={classes.logo} src={logo} alt="logo" />
+            <Typography color="inherit" variant="display2">
+              Welcome to Piccy.
+            </Typography>
+            <Typography color="inherit" variant="subheading" gutterBottom>
+              image sharing for everyone
+            </Typography>
+          </div>
+        </Fade>
       </div>
     );
   }
