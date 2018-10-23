0187390087d41b85c2ce93ecd4ab08bb0a27afa3
Eve Martin-Jones <evemartinjones95@gmail.com>
2018-10-23 13:38:53 +1100

    Add stub methods for login and is valid API

--- server/piccy/src/main/java/com/piccy/demo/ImageController.java
+++ server/piccy/src/main/java/com/piccy/demo/ImageController.java
@@ -46,6 +46,79 @@ public class ImageController {
 	private FileStorageService fileStorageService;
 	private final AtomicLong counter = new AtomicLong();
 
+	
+	public class LoginResponseJson
+	{
+		private String userCookie;
+		private Boolean success;
+		
+		public String getUserCookie() {
+			return userCookie;
+		}
+		
+		public void setUserCookie(final String newValue) {
+			userCookie = newValue;
+		}
+		
+		public Boolean getSuccess() {
+			return success;
+		}
+		
+		public void setSuccess(final Boolean newValue) {
+			success = newValue;
+		}
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public LoginResponseJson login(@RequestParam("username") String username, @RequestParam("password") String password) {
+		// TODO: check these against actual database credentials
+		final LoginResponseJson response = new LoginResponseJson();
+		if (username.equals("user") && password.equals("password")) {
+			// TODO: return an actual value that can be used for a cookie
+			response.setUserCookie(username);
+			response.setSuccess(true);
+		} else {
+			response.setUserCookie("");
+			response.setSuccess(false);
+		}
+		return response;
+	}
+	
+	public class IsValidResponseJson
+	{
+		private String username;
+		private Boolean isValid;
+		
+		public String getUsername() {
+			return username;
+		}
+		
+		public void setUsername(final String newValue) {
+			username = newValue;
+		}
+		
+		public Boolean getIsValid() {
+			return isValid;
+		}
+		
+		public void setIsValid(final Boolean newValue) {
+			isValid = newValue;
+		}
+	}
+	
+	@RequestMapping(value="/isvalid", method = RequestMethod.POST)
+	public IsValidResponseJson isvalid(@RequestParam("usercookie") String usercookie) {
+		final IsValidResponseJson response = new IsValidResponseJson();
+		if (usercookie.length() == 0) {
+			response.setUsername("");
+			response.setIsValid(false);
+			return response;
+		}
+		// TODO: check that the cookie is valid and separate the username from the cookie
+		response.setUsername("user");
+		response.setIsValid(true);
+		return response;
+	}
 
 	@RequestMapping(value = "/upload", method = RequestMethod.POST)
 	public FileResponse upload(@RequestParam("userid") String userid, @RequestParam("file") MultipartFile file) {
